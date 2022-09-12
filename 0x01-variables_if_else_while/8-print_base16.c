5c2416504d3802ba1aaadb01389fe8b13d60e425
Jarchangel <jack.paragon55@gmail.com>
2022-09-12 09:09:59 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all hexadecimal strings
+ *
+ * Return: Always (Sucess)
+ */
+int main(void)
+
+{
+	char c;
+
+	for (c = '0'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
